01cda778c1bb14f85a7cd9353568833b4f6cd005
sudesh.tnbt <sudeshacharya110@gmail.com>
2024-07-16 17:14:48 +0545

    Updated cards with input, added initial losing and winning mechanism.

--- main.py
+++ main.py
@@ -9,18 +9,32 @@ user_score = sum(user_cards)
 dealer_cards = random.choices(cards, weights=None, k=2)
 dealer_score = sum(dealer_cards)
 
+
+def status():
+    return True
+
+
 while not end_of_game:
 
+    # Display stats
     print(f"Your cards: {user_cards}, current score: {user_score}")
     print(f"Computer's first card: {dealer_cards[0]}")
 
+    # User Input
     prompt = input("Type 'y' to get another card, type 'n' to pass: ").lower()
     user_decision = True if (prompt == 'y') else False if (prompt == 'n') else None
 
-    if user_decision:
-        user_cards += random.choices(cards, weights=None)
-        user_score = sum(user_cards)
-
-
-    end_of_game = True
-
+    if user_decision:  # If user decides to take a card
+        user_cards += random.choices(cards, weights=None)  # Add card to the user_cards
+        user_score = sum(user_cards)  # Compute cumulative score
+        if user_score > 21:  # Check if the score crosses threshold
+            print("You went over, you lose.")
+            end_of_game = status()
+        elif user_score == 21:  # Check if the score matches threshold
+            print("You got 21, you win.")
+            end_of_game = status()
+        else:  # Continue if the score is less
+            pass
+    else:
+        while not end_of_game:
+            end_of_game = status()

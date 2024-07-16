8451d2bae781fcd3d667769e19fc740947828124
sudesh.tnbt <sudeshacharya110@gmail.com>
2024-07-16 10:53:01 +0545

    Built mechanism of setup of user's and dealer's card.

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Blackjack Project")
+import random
+
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+end_of_game = False
+
+while not end_of_game:
+
+    user_cards = random.choices(cards, weights= None, k = 2)
+    user_score = sum(user_cards)
+    dealer_cards = random.choices(cards, weights= None, k = 2)
+    dealer_score = sum(dealer_cards)
+
+    print(f"Your cards: {user_cards}, current score: {user_score}")
+    print(f"Computer's first card: {dealer_cards[0]}")
+
+
+    end_of_game = True
+
